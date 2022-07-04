7cf612d16ed6b0c4747750f4d31d03e8d1b38111
Yan K <diskycn@gmail.com>
2022-07-04 19:46:51 +0800

    collect staticfiles

--- kywords/settings.py
+++ kywords/settings.py
@@ -122,6 +122,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+STATICFILES_DIRS = [
+    BASE_DIR / "media",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
