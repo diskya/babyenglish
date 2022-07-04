4483a1e623f038581ee1422751ad6bf63d7393ce
Yan K <diskycn@gmail.com>
2022-07-04 21:09:15 +0800

    server error 500 / 2

--- kywords/settings.py
+++ kywords/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wmy66q=q6%obe*(j^&miqzsn5zmknwm3rhpe)qo@z^8x7@6kfr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
